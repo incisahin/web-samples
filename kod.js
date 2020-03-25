f46a2245ed3b00178831f4cb4960f0587aca1008
incisahin <inci.sahinogullari@gmail.com>
2020-03-25 22:03:59 +0300

    js düzenlemeleri

--- kod.js
+++ kod.js
@@ -1,1 +1,16 @@
-alert("Merhaba Dunya");
+
+
+
+function gosterVeYonlendir(sectionId) {
+    butunSectionKapat();
+    var section = document.getElementById(sectionId);
+    section.style.display = "block";
+}
+function butunSectionKapat() {
+    document.getElementById('vodafone-kampanya').style.display = "none";
+    document.getElementById('dsmart-kampanya').style.display = "none";
+    document.getElementById('TT-kampanya').style.display = "none";
+    document.getElementById('turkcell-kampanya').style.display = "none";
+    document.getElementById('millenicom-kampanya').style.display = "none";
+
+}
